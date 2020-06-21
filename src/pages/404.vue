d9b7ba20ef8322656a326aab44c0645b88ba74d7
Jordan Han <info@jhanrahan.com.au>
2020-06-21 20:18:47 +1000

    added custom 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,12 @@
+<template>
+  <Layout>
+    <div class="container">
+      <h1>Something went wrong!</h1>
+      <p>Click <a href="javascript:history.back()">here</a> to go back, or return <a href="/">home</a>.</p>
+    </div>
+  </Layout>
+</template>
+
+<script>
+  export default {}
+</script>
