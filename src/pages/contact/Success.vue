3d9492c256665527560042d8180cc1aeedd907d7
Jordan Han <info@jhanrahan.com.au>
2020-06-21 19:55:58 +1000

    Added success page for contact form

---
+++ src/pages/contact/Success.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <div class="container">
+      <h1>Thank you!</h1>
+      <p>Your message has been sent and you should recieve a reply within 24 hours.</p>
+      <a href="/contact">Back</a>
+    </div>
+  </Layout>
+</Template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped>
+</style>
