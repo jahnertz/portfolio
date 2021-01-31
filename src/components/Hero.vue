b99388e10181720224a64fab6e665fe3c6a3a6e1
jahnertz <hey@jordanhan.com.au>
2021-01-31 15:41:50 +1100

    removed hero from index

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="hero">
+<!--    <div class="hero">
         <h1 class="hero-title" v-html="settings.hero_title" />
         <h2 class="hero-subtitle" v-html="settings.hero_subtitle" />
-    </div>
+    </div>-->
 </template>
 
 <script>
